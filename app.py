816a4e4b49134d4b4bffe7bb1eff2a0f1343e44b
NicTavares <nic.tavares22@gmail.com>
2020-01-22 20:12:28 -0800

    abc

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ if os.path.isfile('save.txt'):
         apps = [x for x in temp if x.strip()]
 
 
+def deleteApp(b):
+    text = b['text']
+    b.destroy()
+    for app in apps:
+        if app == text:
+            apps.remove(app)
+
+
 
 def addApp():
     for widget in inner_frame.winfo_children():
@@ -22,8 +30,10 @@ def addApp():
                                            filetypes=(("executables", "*.exe"), ("all files", "*.*")))
     apps.append(file_name)
     for app in apps:
-        label = tk.Label(inner_frame, text=app)
-        label.pack()
+        item = tk.Button(inner_frame, text=app, padx=10,
+                         pady=5, fg="white", bg="#263D43")
+        item['command'] = lambda b=item: deleteApp(b)
+        item.pack()
 
 
 def runApps():
@@ -49,8 +59,10 @@ run_a = tk.Button(root, text="Run Apps", padx=10,
 run_a.pack()
 
 for app in apps:
-    label = tk.Label(inner_frame, text=app)
-    label.pack()
+    item = tk.Button(inner_frame, text=app, padx=10,
+                     pady=5, fg="white", bg="#263D43")
+    item['command'] = lambda b=item: deleteApp(b)
+    item.pack()
 
 root.mainloop()
 
