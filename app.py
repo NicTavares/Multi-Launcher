b235b650604f4f02ae0e27a088af7578ff9e9a25
NicTavares <nic.tavares22@gmail.com>
2020-01-22 18:14:14 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+root.title("Multi-Launcher")
+apps = []
+
+if os.path.isfile('save.txt'):
+    with open('save.txt', 'r') as f:
+        temp = f.read()
+        temp = temp.split(',')
+        apps = [x for x in temp if x.strip()]
+
+
+
+def addApp():
+    for widget in inner_frame.winfo_children():
+        widget.destroy()
+
+    file_name = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                           filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    apps.append(file_name)
+    for app in apps:
+        label = tk.Label(inner_frame, text=app)
+        label.pack()
+
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
+
+
+outer_frame = tk.Canvas(root, height=700, width=700, bg="#263D42")
+
+outer_frame.pack()
+
+inner_frame = tk.Frame(root, bg="grey")
+inner_frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10,
+                     pady=5, fg="white", bg="#263D42",  command=addApp)
+openFile.pack()
+
+run_a = tk.Button(root, text="Run Apps", padx=10,
+                     pady=5, fg="white", bg="#263D42", command=runApps)
+
+
+run_a.pack()
+
+for app in apps:
+    label = tk.Label(inner_frame, text=app)
+    label.pack()
+
+root.mainloop()
+
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
